6b9b17a3b24a2d21ddc4dd47eb899261215fd18e
russjr-fullsail <russjr@fullsail.edu>
2014-05-07 19:22:40 -0400

    intro to Python loops

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -32,4 +32,56 @@ characters.append("obi won")
 movies = dict() #create dictionary object
 movies = {"Star Wars":"Darth Vader", "Silence of the Lambs":"Hannibal Lecter"}
 #print movies["Star Wars"]
+'''
+#While Loop----------
+i= 0
+while i<9:
+    print "The count is", i
+    i = i+1
+
+#For Loop---------
+for i in range(0,10):
+    print "The count is", i
+    i = i+1
+'''
+
+#for each loop-----------
+rappers =["Tupac","Nas","Biggie Small"]
+for r in rappers:
+    #print "One of the best rappers is" + r
+    pass
+
+#Functions ----------------
+
+def calcArea(h, w):
+    area = h * w
+    return area
+
+a = calcArea(20, 40);
+#print "My area is " + str(a) + " sqft"
+
+title = "Contact Us"
+body = "You can contact us at contact@us.com"
+message = '''
+<!Doctype html>
+<html>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+    {body}
+    </body>
+</html>
+'''
+
+message = message.format(**locals())
+print message
+
+
+
+
+
+
+
+
 
