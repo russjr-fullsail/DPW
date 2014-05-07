336562a8f1ba672e99cd784ab5e3df3a647d54d9
russjr-fullsail <russjr@fullsail.edu>
2014-05-07 18:58:05 -0400

    Intro to Python Conditionals

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -12,7 +12,24 @@ last_name = "De Frog"
 birth_year = 1974
 current_year = 2014
 age = current_year - birth_year
-print "You are " + str(age) + " years old"
+#print "You are " + str(age) + " years old"
 
+budget = 90
 
+if budget > 100:
+    brand = "nike"
+    print "Yay! we can buy cool " + brand + " shoes!"
+elif budget > 50:
+    #print "We can at least get some generic sneakers."
+    pass
+else:
+    pass
+
+characters = ["leia","luke","chewy","lando"]
+characters.append("obi won")
+#print characters[0]
+
+movies = dict() #create dictionary object
+movies = {"Star Wars":"Darth Vader", "Silence of the Lambs":"Hannibal Lecter"}
+#print movies["Star Wars"]
 
