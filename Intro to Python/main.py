eb1e777949d4bccd41c36110bf42a15af9456d5f
russjr-fullsail <russjr@fullsail.edu>
2014-05-07 18:29:37 -0400

    intro to python video 3 follow along

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -1,1 +1,18 @@
-print "hello world!"
+#one lined comments
+'''
+Doc strings
+'''
+
+first_name = "Kermit"
+last_name = "De Frog"
+
+#response = raw_input("Enter your Name  ")
+#print "Hello there,", response
+
+birth_year = 1974
+current_year = 2014
+age = current_year - birth_year
+print "You are " + str(age) + " years old"
+
+
+
