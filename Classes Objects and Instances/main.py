4cbe27cdc4eb05f39fa50344299accd6f5ed2d48
russjr-fullsail <russjr@fullsail.edu>
2014-05-08 12:25:30 -0400

    demo

--- Classes Objects and Instances/main.py
+++ Classes Objects and Instances/main.py
@@ -4,13 +4,26 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world')
-
+        about_button = Button()
+        about_button.label = "About Us"
+        about_button.show_label()
+        contact_button = Button()
+        contact_button.label = "Contact Us"
+        contact_button.show_label()
+        
 class Button(object):
     def __init__(self):
-        pass
+        self.label = ""
+        #self.on_roll_over("Hello!!")
 
     def click(self):
+        print "I've been clicked"
+
+    def on_roll_over(self, message):
+        print "You've rolled over my button" + message
+
+     def show_label(self):
+         print "My label is" + self.label
 
 app = webapp2.WSGIApppplication([
     ('/', MainHandler)
