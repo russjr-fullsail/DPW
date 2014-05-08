4819f5da342687ecf156b339653fb3619ed37ad5
russjr-fullsail <russjr@fullsail.edu>
2014-05-08 14:10:27 -0400

    classes

--- classes-html/main.py
+++ classes-html/main.py
@@ -8,6 +8,7 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
+        p.body = "Miss Piggy like Kermit De Frog!"
         self.response.write (p.print_out())
 
 class Page(object):
