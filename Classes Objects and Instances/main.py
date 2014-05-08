573dacaa3dc56103f488946a523bb30fb27b7bff
russjr-fullsail <russjr@fullsail.edu>
2014-05-08 12:12:07 -0400

    demo

--- Classes Objects and Instances/main.py
+++ Classes Objects and Instances/main.py
@@ -1,2 +1,17 @@
 __author__ = 'russjr'
 
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world')
+
+class Button(object):
+    def __init__(self):
+        pass
+
+    def click(self):
+
+app = webapp2.WSGIApppplication([
+    ('/', MainHandler)
+], debug=True)
