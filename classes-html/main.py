919f57cbe68e6369cb9e2aab2bc439cebb7150e2
russjr-fullsail <russjr@fullsail.edu>
2014-05-08 14:17:26 -0400

    setup pages

--- classes-html/main.py
+++ classes-html/main.py
@@ -4,37 +4,13 @@ Russell Chatham
 Web App
 '''
 import webapp2
+from pages import Page #from package import Class
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
         p.body = "Miss Piggy like Kermit De Frog!"
         self.response.write (p.print_out())
-
-class Page(object):
-    def __init__(self):
-        self.title = "Welcome!"
-        self.css = "css/style.css"
-        self.head = """
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>{self.title}</title>
-        <link href="{self.css}" rel="Stylesheet" type="text/css" />
-    </head>
-    <body>
-        """
-
-        self.body = "Welcome to my Python Page!"
-        self.close = """
-    </body>
-</html>
-        """
-
-    def print_out(self):
-        all = self.head + self.body + self.close
-        all = all.format(**locals())
-        return all
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
---
+++ classes-html/pages.py
@@ -0,0 +1,26 @@
+__author__ = 'russjr'
+
+class Page(object):
+    def __init__(self):
+        self.title = "Welcome!"
+        self.css = "css/style.css"
+        self.head = """
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>{self.title}</title>
+        <link href="{self.css}" rel="Stylesheet" type="text/css" />
+    </head>
+    <body>
+        """
+
+        self.body = "Welcome to my Python Page!"
+        self.close = """
+    </body>
+</html>
+        """
+
+    def print_out(self):
+        all = self.head + self.body + self.close
+        all = all.format(**locals())
+        return all
