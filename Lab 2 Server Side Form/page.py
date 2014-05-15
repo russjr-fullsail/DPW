f24a76fde3610652d3d2ac11907b19bd541cb8d2
russjr-fullsail <russjr@fullsail.edu>
2014-05-15 16:45:03 -0400

    setup of html page

---
+++ Lab 2 Server Side Form/page.py
@@ -0,0 +1,22 @@
+'''
+Russell Chatham Jr.
+DPW
+Server Side Form
+5/15/14
+'''
+
+def __init__(self):
+		self.header ='''<!DOCTYPE>
+<html>
+    <head>
+        <title>Welcome To Vehicle Chooser</title>
+        <link rel="stylesheet" href="css/main.css" type="text/css" />
+    </head>
+    <body>
+    <header>
+		<h1>Please Select Vehicle"></h1>
+	</header>
+       '''
+		self.registration = form = '''
+
+
