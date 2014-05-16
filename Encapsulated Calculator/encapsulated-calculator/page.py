0efbeeceaee92a11c2056eb3e715c325b08c1687
russjr-fullsail <russjr@fullsail.edu>
2014-05-15 22:20:10 -0400

    setting up movie links

--- Encapsulated Calculator/encapsulated-calculator/page.py
+++ Encapsulated Calculator/encapsulated-calculator/page.py
@@ -17,4 +17,10 @@ class Page():
             <div id="container">
             <h1>Star Wars Theater Sales</h1>
 
-        
+        self.links = '''<a href="?movie=0" class="links">Star Wars - Episode IV - A New Hope (1977)"</a>
+        <a href="?movie=1" class="links">Star Wars - Episode V - Empire Strikes Back (1980) </a>
+		<a href="?movie=2" class="links">Star Wars - Episode VI - Return of the Jedi (1983)</a>
+		<a href="?movie=3" class="links">Star Wars - Episode I - The Phantom Menace (1999)</a>
+		<a href="?movie=4" class="links">Star Wars - Episode II - Attack of the Clones (2002)</a>
+        <a href="?movie=5" class="links">Star Wars - Episode III - Revenge of the Sith (2005)</a>
+		<a href="?movie=6" class="links">Star Wars - The Clone Wars (2008)</a>
