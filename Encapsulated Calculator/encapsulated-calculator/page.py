6dd3b05b4ea5fc8d522fe14173825ea856436a56
russjr-fullsail <russjr@fullsail.edu>
2014-05-15 22:13:44 -0400

    building html page

---
+++ Encapsulated Calculator/encapsulated-calculator/page.py
@@ -0,0 +1,20 @@
+'''
+Russell Chatham Jr.
+DPW
+Encapsulated Calculator
+5/15/14
+'''
+class Page():
+    def __init__(self):
+        self.header = '''<!DOCTYPE>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title>Star Wars Movie Sales</title>
+            <link rel="stlesheet" href="css/main.css" type="text/css" />
+        </head>
+        <body>
+            <div id="container">
+            <h1>Star Wars Theater Sales</h1>
+
+        
