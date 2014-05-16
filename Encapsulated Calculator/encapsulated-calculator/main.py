f5ae2ecb7b0ec6796e2df270ddc48125398ba451
russjr-fullsail <russjr@fullsail.edu>
2014-05-15 21:34:07 -0400

    start of project

--- Encapsulated Calculator/encapsulated-calculator/main.py
+++ Encapsulated Calculator/encapsulated-calculator/main.py
@@ -12,7 +12,18 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         page = Page() #creates an instance of the Page function
 
-       
+        #Star Wars Episode IV (1977)
+        self.epiv = Movie()
+        self.epiv.title = "Star Wars Episode IV"
+        self.epiv.ustheater = 460000000
+        self.epiv.eutheater = 314000000
+        self.epiv.calc_total_sales()
+        print "The total number of ticket sales for " +self.epiv.title+ " is " +str(self.epiv.total_sales)
+
+        
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
