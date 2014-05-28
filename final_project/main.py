ba9ae5c74ae8b44d5bfcff7b2504c77c35051522
russjr-fullsail <russjr@fullsail.edu>
2014-05-28 19:59:53 -0400

    setting up import

--- final_project/main.py
+++ final_project/main.py
@@ -5,6 +5,10 @@ Application with API
 5/28/14
 '''
 import webapp2
+import urllib2 #python classes and code for requesting info, receiving info and opening
+from xml.dom import minidom
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
