c9fb0c4ed29115e94e5e33cf1bc87255e3142e69
russjr-fullsail <russjr@fullsail.edu>
2014-05-28 20:03:24 -0400

    setting up the form page

--- final_project/main.py
+++ final_project/main.py
@@ -8,7 +8,11 @@ import webapp2
 import urllib2 #python classes and code for requesting info, receiving info and opening
 from xml.dom import minidom
 
-
+class MainHandler(webapp2.RequestHandler):#controller class that is collecting and sending information from the view to model class
+    def get(self):
+        p = FormPage()
+        p.inputs = [['city', 'text', 'City'],['state', 'text', 'State'],['Submit', 'submit']]
+        
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
