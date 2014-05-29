96df14e3c515592320ec0ae8f7a12a58e81fc3af
russjr-fullsail <russjr@fullsail.edu>
2014-05-28 20:08:49 -0400

    setup if statement and data

--- final_project/main.py
+++ final_project/main.py
@@ -12,7 +12,20 @@ class MainHandler(webapp2.RequestHandler):#controller class that is collecting a
     def get(self):
         p = FormPage()
         p.inputs = [['city', 'text', 'City'],['state', 'text', 'State'],['Submit', 'submit']]
-        
+
+        if self.request.GET: #will only work if city and state are in url
+            #gets the info from the API and creates the model
+            hm = HouseModel()
+            hm.city = self.request.GET['city']
+            hm.state = self.request.GET['state']
+            hm.callApi()
+            #takes the data from model class and sends it to the view class
+            hv = HouseView()
+            hv.housedo = hm.houses
+            p._body = hv.content
+
+        self.response.write(p.print_out())
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
