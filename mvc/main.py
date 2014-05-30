d3e372663f0c800eb337ba19aaba76caede5caad
russjr-fullsail <russjr@fullsail.edu>
2014-05-29 21:51:17 -0400

    mvc demo

--- mvc/main.py
+++ mvc/main.py
@@ -4,11 +4,29 @@ DPW
 5/29/14
 MVC
 '''
+
 import webapp2
+import urllib2
+from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = FormPage()
+        p.inputs = [  ['zip', 'text','zip code'],['Submit', 'submit']]
+
+
+        if self.request.GET:
+
+            wm = WeatherModel() #creates our Model
+            wm.zip = self.request.GET['zip'] #sends our zip from the url to our Model
+            wm.callApi() #tells it to connect to the API
+            wv = WeatherView() #creates our View
+            wv.wdos = wm.dos #takes data objects form Model and gives them to View
+            p._body = wv.content
+
+            #self.response.write(xmldoc.getElementsByTagName('title')[2].firstChild.nodeValue)
+        self.response.write(p.print_out())
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
