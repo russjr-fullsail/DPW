b9c203b240e4be942db9a2ef0f795224ee98c91e
russjr-fullsail <russjr@fullsail.edu>
2014-05-29 21:40:43 -0400

    MVC Demo

---
+++ mvc/main.py
@@ -0,0 +1,15 @@
+'''
+Russell Chatham Jr.
+DPW
+5/29/14
+MVC
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
