b1534109ccf2626fdae3e96b2258e175213db379
russjr-fullsail <russjr@fullsail.edu>
2014-05-29 21:57:45 -0400

    set import urllib2 and xml.dom

--- final-project-application-with-api/main.py
+++ final-project-application-with-api/main.py
@@ -4,12 +4,12 @@ DPW
 Final Project Application with API
 5/29/14
 '''
-
 import webapp2
+import urllib2 #python classes and code needed to requesting info, receiving, and opening
+from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+    '''class controller for collecting and sending info from the view to model class'''
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
