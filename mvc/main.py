0ab2125fb131113dc2b53ba4c02e2179b69aaaa3
russjr-fullsail <russjr@fullsail.edu>
2014-05-29 21:51:41 -0400

    mvc demo save

--- mvc/main.py
+++ mvc/main.py
@@ -27,6 +27,69 @@ class MainHandler(webapp2.RequestHandler):
             #self.response.write(xmldoc.getElementsByTagName('title')[2].firstChild.nodeValue)
         self.response.write(p.print_out())
 
+class WeatherView(object):
+    '''This class handles how the data is shown to the user'''
+    def __init__(self):
+        self.__wdos = []
+        self.__content = '<br />'
+
+    def update(self):
+        for do in self.__wdos:
+            self.__content += do.day + "      HIGH:  " + do.high + "      Low:  " + do.low + "      CONDITION:  "+ do.condition + '<br />'
+
+    @property
+    def content(self):
+        return self.__content
+
+    @property
+    def wdos(self):
+        pass
+
+    @wdos.setter
+    def wdos(self, arr):
+        self.__wdos = arr
+        self.update()
+
+class WeatherModel(object):
+    '''This model handels fetching, parsing and sorting data form Yahoo's weather APT'''
+    def __init__(self):
+        self.__url = "http://xml.weather.yahoo.com/forecastrss?p="
+        self.__zip = ''
+        self.__xmldoc = ''
+
+
+    def callApi(self):
+        request = urllib2.Request(self.__url+self.__zip)
+        opener = urllib2.build_opener()
+        result = opener.open(request)
+        self.__xmldoc = minidom.parse(result)
+
+        self.content = '<br/>'
+        list = self.__xmldoc.getElementsByTagName("yweather:forecast")
+        self._dos = []
+
+        for tag in list:
+            do = WeatherData()
+            do.day = tag.attributes['day'].value
+            do.high = tag.attributes['high'].value
+            do.low = tag.attributes['low'].value
+            do.code = tag.attributes['code'].value
+            do.condition = tag.attributes['text'].value
+            do.date = tag.attributes['date'].value
+            self._dos.append(do)
+
+    @property
+    def dos(self):
+        return self._dos
+
+    @property
+    def zip(self):
+        pass
+
+    @zip.setter
+    def zip(self, z):
+        self.__zip = z
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
